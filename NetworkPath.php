8ecffe6b365d46e825e947cda4ad1d64a8b961d7
Corey Tian <haohetian@gmail.com>
2021-01-11 22:50:05 +1100

    Added load CSV function

--- NetworkPath.php
+++ NetworkPath.php
@@ -1,9 +1,21 @@
 <?php
 
 class NetworkPath {
+    protected $connectionsData;
 
+    /**
+     * Load the csv file data and store the connections data into an array
+     * e.g. $this->connectionsData["A"]["B"] = 10;
+     *
+     * @param string       $filePath  The csv file path
+     */
     public function loadCSV($filePath){
-        echo "loadCSV";
+        $file = fopen($filePath, 'r');
+        while (($line = fgetcsv($file)) !== FALSE) {
+            $this->connectionsData[$line[0]][$line[1]] = $line[2];
+        }
+        //var_dump($this->csvData);
+        fclose($file);
     }
 
     public function findPath(){
