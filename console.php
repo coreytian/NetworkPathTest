3fd9f3cce1215ad41b0e49e52673c53da3cf4664
Haohe Tian <haohetian@192-168-1-3.tpgi.com.au>
2021-01-11 22:36:34 +1100

    Added the console I/O functionailty

--- console.php
+++ console.php
@@ -2,12 +2,24 @@
 require('NetworkPath.php');
 
 $filePath = $argv[1];
-//TODO: Verify the argument
+// TODO: Verify the argument
 
 $networkPath =  new NetworkPath();
 $networkPath->loadCSV($filePath);
-$networkPath->findPath();
 
+$handle = fopen ("php://stdin","r");
 
+// Continually wait for user input
+while($line = trim(fgets($handle))){
+    // Enter QUIT/quit to terminate 
+    if(strtoupper($line) == 'QUIT'){ 
+        exit("Bye!\n");
+    }
 
+    // TODO: Verify the input format and value
+    $lineVars = explode(' ', $line);
+    //var_dump($lineVars);
+    $networkPath->findPath(); 
+    echo "\n";
+}
 
